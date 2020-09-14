93e877bd48ce09e7af17653acd3f30db6a921170
rico0072010 <danildolin96@gmail.com>
2020-09-14 21:41:14 +0500

    Add ajax functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,11 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex_client[:sandbox_secret_api],
       endpoint: Rails.application.credentials.iex_client[:sandbox_endpoint]
     )
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
